2ae5319be2dfe82be5d78da8ab9b9059b1013200
Vitaly Osipov <vital-nvl@mail.ru>
2022-09-24 20:34:39 +0300

    Row min sum elements

---
+++ Work2/Program.cs
@@ -0,0 +1,50 @@
+﻿// Задайте прямоугольный двумерный массив. 
+// Напишите программу, которая будет находить строку с наименьшей суммой элементов.
+
+int[,] GetArray(int rows, int colums)
+{
+    int[,] array = new int[rows, colums];
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = new Random().Next(0, 10);
+            Console.Write($"{array[i, j]} ");
+        }
+        Console.WriteLine();
+
+    }
+    return array;
+}
+
+void MinSumElements(int[,] array)
+{
+    int minRow = 0;
+    int minSumRow = 0;
+    int sumRow = 0;
+    for (int i = 0; i < array.GetLength(1); i++)
+    {
+        minRow += array[0, i];
+    }
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++) sumRow += array[i, j];
+        if (sumRow < minRow)
+        {
+            minRow = sumRow;
+            minSumRow = i;
+        }
+        sumRow = 0;
+    }
+    Console.Write($"Минимальная сумма элементов в {minSumRow + 1} строке");
+}
+
+Console.Write("Введите количество строк массива: ");
+int rows = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите количество столбцов массива: ");
+int colums = Convert.ToInt32(Console.ReadLine());
+
+int[,] array = GetArray(rows, colums);
+Console.WriteLine();
+MinSumElements (array);
