142d03d01d318389a49a6ef971cba3dfe33a88d7
Vitaly Osipov <vital-nvl@mail.ru>
2022-09-25 20:46:34 +0300

    3Darray string

---
+++ Work4/Program.cs
@@ -0,0 +1,34 @@
+﻿// Сформируйте трёхмерный массив из неповторяющихся двузначных чисел. 
+// Напишите программу, которая будет построчно выводить массив, 
+// добавляя индексы каждого элемента.
+
+int[,,] GetArray(int x, int y, int z)
+{
+    int[,,] array = new int[x, y, z];
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            for (int k = 0; k < array.GetLength(2); k++)
+            {
+                array[i, j, k] = new Random().Next(10, 100);
+                Console.Write($"{array[i, j, k]} ({i},{j},{k})  ");
+            }
+            Console.WriteLine();
+        }
+
+    }
+    return array;
+}
+
+Console.Write("Введите размер массива  (x, y, z): ");
+int x = Convert.ToInt32(Console.ReadLine());
+
+int y = Convert.ToInt32(Console.ReadLine());
+
+int z = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine();
+
+int[,,] array = GetArray(z, y, z);
+
+
