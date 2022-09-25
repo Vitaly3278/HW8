93d15975ac4e3217e210deca51f208cee8578888
Vitaly Osipov <vital-nvl@mail.ru>
2022-09-25 21:01:52 +0300

    Spiral Array

---
+++ Work5/Program.cs
@@ -0,0 +1,42 @@
+﻿// Напишите программу, которая заполнит спирально массив 4 на 4.
+
+Console.Write("Введите размер массива: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine();
+
+int[,] array = new int[n, n];
+
+int temp = 1;
+int i = 0;
+int j = 0;
+
+while (temp <= array.GetLength(0) * array.GetLength(1))
+{
+    array[i, j] = temp;
+    temp++;
+    if (i <= j + 1 && i + j < array.GetLength(1) - 1)
+        j++;
+    else if (i < j && i + j >= array.GetLength(0) - 1)
+        i++;
+    else if (i >= j && i + j > array.GetLength(1) - 1)
+        j--;
+    else
+        i--;
+}
+
+WriteArray(array);
+
+void WriteArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            if (array[i, j] / 10 <= 0)
+                Console.Write($" {array[i, j]} ");
+
+            else Console.Write($"{array[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
