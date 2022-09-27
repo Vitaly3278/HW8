811be63ad450882eb28e099b9e959cfe7f42f14b
Vitaly Osipov <vital-nvl@mail.ru>
2022-09-27 22:55:37 +0300

    3Darray string

--- Work4/Program.cs
+++ Work4/Program.cs
@@ -2,6 +2,25 @@
 // Напишите программу, которая будет построчно выводить массив, 
 // добавляя индексы каждого элемента.
 
+bool SearchElements(int[,,] array, int value)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            for (int k = 0; k < array.GetLength(2); k++)
+            {
+                if (array[i, j, k] == value)
+                {
+                    return true;
+                }
+            }
+        }
+
+    }
+    return false;
+}
+
 int[,,] GetArray(int x, int y, int z)
 {
     int[,,] array = new int[x, y, z];
@@ -11,8 +30,22 @@ int[,,] GetArray(int x, int y, int z)
         {
             for (int k = 0; k < array.GetLength(2); k++)
             {
-                array[i, j, k] = new Random().Next(10, 100);
-                Console.Write($"{array[i, j, k]} ({i},{j},{k})  ");
+                bool flag = false;
+                while (flag != true)
+                {
+                    flag = false;
+                    int temp = new Random().Next(10, 100);
+                    if (SearchElements(array, temp))
+                    {
+                        continue;
+                    }
+                    else
+                    {
+                        array[i, j, k] = temp;
+                        Console.Write($"{array[i, j, k]} ({i},{j},{k})  ");
+                        flag = true;
+                    }
+                }
             }
             Console.WriteLine();
         }
